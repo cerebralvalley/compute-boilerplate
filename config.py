b054d02e108bab7baa0390c3f8339565fe70933c
Raymond Del Vecchio <raydelv@MackTruck.local>
2024-10-05 12:30:22 +0100

    gpt2 base

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ import os
 dotenv.load_dotenv()
 
 class Config:
-    MODEL_NAME: str = "mistralai/Mistral-7B-Instruct-v0.2"
+    MODEL_NAME: str = "openai-community/gpt2"
     PORT: int = 8000
     MAX_TOKENS: int = 100
     HUGGINGFACE_ACCESS_TOKEN: str = os.getenv("HUGGINGFACE_ACCESS_TOKEN", "")
+    
