120240f6add947be1b37d145a7d7c3cf001ced1e
Raymond Del Vecchio <raydelv@MackTruck.local>
2024-10-05 10:16:20 +0100

    setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,3 +10,11 @@ sudo apt-get install -y python3 python3-pip
 echo "===== INSTALLING PYTHON DEPENDENCIES ====="
 pip3 install -r requirements.txt
 echo "===== ALL DEPENDENCIES HAVE BEEN INSTALLED SUCCESSFULLY ====="
+echo "===== PRINTING SYSTEM SPECIFICATIONS ====="
+echo "===== GPU ====="
+nvidia-smi
+echo "===== DISK ====="
+df -h
+echo "===== MEMORY ====="
+free -h
+echo "===== SETUP COMPLETE ====="
