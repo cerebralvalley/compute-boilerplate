9e63ca5617db355f7bd8b6b791b2c14a330a4a54
Raymond Del Vecchio <xraydelv@gmail.com>
2024-10-05 15:58:22 +0100

    better serialize

--- utils.py
+++ utils.py
@@ -8,17 +8,16 @@ import json
 from enum import Enum
 from pydantic import BaseModel
 
-class EnumEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Enum):
-            return obj.value
-        return super().default(obj)
-
 def serialize(request: BaseModel) -> str:
     """
     Turns a pydantic model (request param) into serialized JSON string format.
     Uses the EnumEncoder class to get the value of the Enum so we can model dump without error.
     """
+    class EnumEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, Enum):
+                return obj.value
+            return super().default(obj)
     return json.dumps(request.model_dump(), cls=EnumEncoder)
 
 def estimate_model_size(model_name):
